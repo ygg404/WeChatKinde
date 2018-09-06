8ab5c294324bb8321b36635260a4f88f6a45271b
ygg404 <2279834998@qq.com>
2018-09-06 10:54:29 +0800

    ygg

--- utils/util.js
+++ utils/util.js
@@ -1,3 +1,6 @@
+var MD5 = require('../lib/md5.js');
+
+//日期
 const formatTime = date => {
   const year = date.getFullYear()
   const month = date.getMonth() + 1
@@ -16,4 +19,46 @@ const formatNumber = n => {
 
 module.exports = {
   formatTime: formatTime
+}
+
+//签名
+function getSign(signToken, timestamp, nonce, staffid, data, appSecret) {
+  var signStr = "" + timestamp + nonce + staffid + signToken + data + appSecret;
+  console.log("signStr:" + signStr);
+  var signString = MD5.md5(signStr);
+  signString = signString.toUpperCase();
+  console.log('signString:' + signString);
+  return signString;
+}
+
+module.exports = {
+  getSign: getSign
+}
+
+//提示窗口(标题，内容, (错误提示=0 红色，普通提示=1，蓝色) )
+function TipModel(Title, content, level = 1) {
+  var mTitle = Title;
+  var mContent = content;
+  var mColor
+
+  if (level == 0) {
+    mColor = '#F21C2E'
+  } else {
+    mColor = '#075FA9'
+  }
+  wx.showModal({
+    title: mTitle,
+    content: mContent,
+    confirmColor: mColor,
+    showCancel: false,
+    success: function (res) {
+      if (res.confirm) {
+        console.log('用户点击确定')
+      }
+    }
+  });
+}
+
+module.exports = {
+  TipModel: TipModel
 }
